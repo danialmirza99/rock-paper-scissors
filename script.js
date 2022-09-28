a74f2800f8029b41624b2469e5afa87f4bc75084
danialmirza99 <danialmirza99@gmail.com>
2022-09-28 14:09:53 -0700

    changes

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-function userInput(){
-    var input = document.getElementById("user-input").value;
-    if(input == "r"){
-        alert("You chose Rock");
-    }
-    else if(input == "p"){
-        alert("You chose Paper");
-    }
-
-    else if(input == "s"){
-        alert("You chose Scissors");
-    }
-    else{
-        alert("You chose an invalid input. Remember to type r, p, or s. They should all be lowercase");
-    }
-}
 
