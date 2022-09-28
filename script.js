3fdf1fb1946fe1706b5231e88187d0ea2a992484
danialmirza99 <danialmirza99@gmail.com>
2022-09-28 12:21:50 -0700

    added user validation

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function userInput(){
     var input = document.getElementById("user-input").value;
-    alert(input);
+    if(input == "r"){
+        alert("You chose Rock");
+    }
+    else if(input == "p"){
+        alert("You chose Paper");
+    }
+
+    else if(input == "s"){
+        alert("You chose Scissors");
+    }
+    else{
+        alert("You chose an invalid input. Remember to type r, p, or s. They should all be lowercase");
+    }
 }
 
